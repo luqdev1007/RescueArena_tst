6a3f8bd14a1301be83a99a7daf6ff213b9cd93b1
LuQmu5 <luqdev1007@gmail.com>
2026-09-05 23:14:53 +0400

    Update PlayerHealth.cs
    disable player if dead

--- Assets/_Project/Scripts/Player/PlayerHealth.cs
+++ Assets/_Project/Scripts/Player/PlayerHealth.cs
@@ -18,6 +18,7 @@ namespace WebGLRescueArena
             {
                 dead = true;
                 GameEvents.RaisePlayerDied();
+                gameObject.SetActive(false);
             }
         }
     }
